1370679c45129990c5d27ecc0d98f75668aa1c59
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-15 19:06:02 +0200

    added header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.scss';
+import Logo from '../../static/Logo.svg';
+import { Button } from '../Button';
+
+export const Header: React.FC = () => (
+  <header className="header container">
+    <div className="header__container">
+      <img
+        src={Logo}
+        alt="The logo of TESTTASK, containing cat"
+        className="header__logo"
+      />
+      <ul className="header__btns">
+        <li className="header__btn">
+          <Button>Users</Button>
+        </li>
+        <li className="header__btn">
+          <Button>Sign up</Button>
+        </li>
+      </ul>
+    </div>
+  </header>
+);
