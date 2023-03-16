35061b5db9b0928dccc02275e840e12752771a19
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-16 19:33:50 +0200

    added favicons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,27 +3,29 @@ import './Header.scss';
 import Logo from '../../static/Logo.svg';
 
 export const Header: React.FC = () => (
-  <header className="header container">
-    <div className="header__container">
-      <a href="/">
-        <img
-          src={Logo}
-          alt="The logo of TESTTASK, containing cat"
-          className="header__logo"
-        />
-      </a>
-      <ul className="header__btns">
-        <li className="header__btn">
-          <a className="btn" type="button" href="#people">
-            Users
-          </a>
-        </li>
-        <li className="header__btn">
-          <a className="btn" type="button" href="#sign-up">
-            Sign up
-          </a>
-        </li>
-      </ul>
+  <header className="header">
+    <div className="container">
+      <div className="header__container">
+        <a href="/">
+          <img
+            src={Logo}
+            alt="The logo of TESTTASK, containing cat"
+            className="header__logo"
+          />
+        </a>
+        <ul className="header__btns">
+          <li className="header__btn">
+            <a className="btn" type="button" href="#people">
+              Users
+            </a>
+          </li>
+          <li className="header__btn">
+            <a className="btn" type="button" href="#sign-up">
+              Sign up
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 );
