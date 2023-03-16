b745034d02d1c59b7dab9bc5231218f9455763ad
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-16 16:59:17 +0200

    added post request

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import cn from 'classnames';
 
 import { SectionTitle } from '../SectionTitle';
@@ -6,6 +6,7 @@ import { SignUpField } from './SignUpField/SignUpField';
 import './SignUp.scss';
 import { SignUpRadio } from './SignUpRadio/SignUpRadio';
 import { SignUpFile } from './SignUpFile/SignUpFile';
+// import { postNewUser } from '../../api/users';
 
 const allowedFileFormats = ['image/jpeg', 'image/jpg'];
 
@@ -14,7 +15,7 @@ export const SignUp = () => {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [positionId, setPositionId] = useState(0);
-  const [file, setFile] = useState<File | null>(null);
+  const [photo, setPhoto] = useState<File | null>(null);
 
   // Error messages
   const [errorName, setErrorName] = useState('');
@@ -22,10 +23,15 @@ export const SignUp = () => {
   const [errorPhone, setErrorPhone] = useState('');
   const [errorFile, setErrorFile] = useState('');
 
+  const img = useMemo(() => new Image(), []);
+  if (photo) {
+    img.src = URL.createObjectURL(photo);
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (!name || !email || !phone || !file) {
+    if (!name || !email || !phone || !photo) {
       return;
     }
 
@@ -40,25 +46,51 @@ export const SignUp = () => {
     const isEmailValid =
       /^[a-zA-Z0-9._%+-]{2,100}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,100}$/.test(email);
     const isPhoneValid = /^\+380\d{9}$/.test(phone);
+
+    const imgArea = img.height * img.width;
+
     const isFileValid =
-      allowedFileFormats.includes(file.type) && file.size > 70 * 70;
+      allowedFileFormats.includes(photo.type) &&
+      photo.size / 1024 <= 5000 &&
+      imgArea > 70 * 70;
 
     // Setting error messages
     if (!isNameValid) {
       setErrorName('Letter only, length from 2 to 60');
+      return;
     }
 
     if (!isEmailValid) {
       setErrorEmail('Invalid email');
+      return;
     }
 
     if (!isPhoneValid) {
       setErrorPhone('Check the format +38 (0XX) XXX - XX - XX');
+      return;
     }
 
     if (!isFileValid) {
-      setErrorFile('Minimal size is 70x70, formats: jpg or jpeg');
+      setErrorFile('Minimal size is 70x70, formats: jpg or jpeg, <5MB');
+      return;
     }
+
+    const newUser = new FormData();
+
+    newUser.append('name', name);
+    newUser.append('email', email);
+    newUser.append('phone', phone);
+    newUser.append('position_id', String(positionId));
+    newUser.append('photo', photo);
+
+    const TOKEN =
+      'eyJpdiI6IlkxZG9DNDZmaFQ3QjMrWFFaOVBvQUE9PSIsInZhbHVlIjoiY3p2R3F2dWxESEpvclwvbTRKaUNqMHZuanI3bDgyaHprZnpsMUVCaFAzUG84aXZPYW1tS3NEOWMwRGo1TU5KNkR2R0lzeUNPTDZRUU1vcTRDZDR2a3BnPT0iLCJtYWMiOiIyZTMzMjc4YTU0YTc5M2FmY2JkNDIyNjA1MDdjZDllZmI0OWJhYzBmOWI1NTdlMDVjZTYwM2JkYjAyNWM3NDlmIn0=';
+
+    fetch('https://frontend-test-assignment-api.abz.agency/api/v1/users', {
+      method: 'POST',
+      body: newUser,
+      headers: { Token: TOKEN },
+    }).then((res) => console.log(res));
   };
 
   return (
@@ -88,11 +120,11 @@ export const SignUp = () => {
 
           <SignUpRadio value={positionId} updateValue={setPositionId} />
 
-          <SignUpFile updateFile={setFile} errorMessage={errorFile} />
+          <SignUpFile updateFile={setPhoto} errorMessage={errorFile} />
 
           <button
             className={cn('sign-up__submit btn', {
-              'btn--disabled': !name || !email || !phone || !file,
+              'btn--disabled': !name || !email || !phone || !photo,
             })}
           >
             Sign up
